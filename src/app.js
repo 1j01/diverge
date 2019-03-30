aa3066af428bf772f4b8a6c9bc3a8c799492510b
Isaiah Odhner <isaiahodhner@gmail.com>
2019-03-29 23:08:35 -0400

    Use let and const

--- src/app.js
+++ src/app.js
@@ -9,14 +9,13 @@
 // and if we're positioning the input, maybe we can just use that
 // and get pointer-based selection and other benefits
 
-var input = document.getElementById("input");
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+const input = document.getElementById("input");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-var paths = [];
-var current_path = [];
+let paths = [];
 
-var providers = [
+const providers = [
 	new DatabaseProvider(),
 	// new MarkovProvider({
 	// 	order: 3,
@@ -25,10 +24,10 @@ var providers = [
 	new OriginalJokeProvider(),
 ];
 
-var query_providers = function(){
-	var path_strings = [];
-	for (var i = 0; i < providers.length; i++) {
-		var provider = providers[i];
+const query_providers = function(){
+	let path_strings = [];
+	for (let i = 0; i < providers.length; i++) {
+		const provider = providers[i];
 		path_strings = path_strings.concat(provider.query(input.value, input.selectionStart));
 	}
 
@@ -36,15 +35,15 @@ var query_providers = function(){
 	// i.e. for when there are multiple paths shown as one since it branches off out of the view
 	// or just for when there are branches in general
 
-	var old_paths_left = paths;
-	var path_strings_left = path_strings;
+	let old_paths_left = paths;
+	let path_strings_left = path_strings;
 	paths = [];
 
-	for (var j = 0; j < path_strings_left.length; j++) {
-		var path_string = path_strings_left[j];
-		for (var k = 0; k < old_paths_left.length; k++) {
-			var old_path = old_paths_left[k];
-			if(old_path.string == path_string){
+	for (let j = 0; j < path_strings_left.length; j++) {
+		let path_string = path_strings_left[j];
+		for (let k = 0; k < old_paths_left.length; k++) {
+			const old_path = old_paths_left[k];
+			if(old_path.string === path_string){
 				paths.push(old_path);
 				path_strings_left.splice(j, 1); j--;
 				old_paths_left.splice(k, 1); k--;
@@ -53,12 +52,12 @@ var query_providers = function(){
 		}
 	}
 
-	for (var j = 0; j < path_strings_left.length; j++) {
-		var path_string = path_strings_left[j];
-		for (var k = 0; k < old_paths_left.length; k++) {
-			var old_path = old_paths_left[k];
+	for (let j = 0; j < path_strings_left.length; j++) {
+		const path_string = path_strings_left[j];
+		for (let k = 0; k < old_paths_left.length; k++) {
+			const old_path = old_paths_left[k];
 			// if(old_path.string.toLowerCase() == path_string.toLowerCase()){
-			if(old_path.string[0] == path_string[0]){
+			if(old_path.string[0] === path_string[0]){
 				old_path.set_string(path_string);
 				paths.push(old_path);
 				path_strings_left.splice(j, 1); j--;
@@ -68,34 +67,34 @@ var query_providers = function(){
 		}
 	}
 
-	for (var j = 0; j < path_strings_left.length; j++) {
-		var path_string = path_strings_left[j];
-		var path = new Path(path_string);
+	for (let j = 0; j < path_strings_left.length; j++) {
+		const path_string = path_strings_left[j];
+		const path = new Path(path_string);
 		paths.push(path);
 	}
 };
 
-var view_center_x = 0;
+let view_center_x = 0;
 
-var font_size = 20;
-var line_height = 25;
+const font_size = 20;
+const line_height = 25;
 
-var cursor_blink_timer = 0;
-var cursor_blink_on = true;
+let cursor_blink_timer = 0;
+let cursor_blink_on = true;
 
-var previous_text = "";
-var previous_selection_end = 0;
+let previous_text = "";
+let previous_selection_end = 0;
 
-var glyph_canvas_map = new Map;
-var get_glyph_canvas = function(char) {
+const glyph_canvas_map = new Map;
+const get_glyph_canvas = function(char) {
 	if (glyph_canvas_map.has(char)) {
 		return glyph_canvas_map.get(char);
 	} else {
-		var glyph_canvas = document.createElement("canvas");
-		var glyph_ctx = glyph_canvas.getContext("2d");
+		const glyph_canvas = document.createElement("canvas");
+		const glyph_ctx = glyph_canvas.getContext("2d");
 		// ctx.font = font_size + "px/" + line_height + "px Arial";
 		glyph_ctx.font = font_size + "px Arial";
-		var width = glyph_ctx.measureText(char).width;
+		const width = glyph_ctx.measureText(char).width;
 		// TODO: use width based the surrounding characters for kerning
 		// the +5 to width below is mainly for f
 		glyph_canvas.glyph_width = width;
@@ -115,8 +114,8 @@ var get_glyph_canvas = function(char) {
 function Path(string) {
 	this.string = string;
 	this.glyphs = [];
-	for (var j = 0; j < string.length; j++) {
-		var char = string[j];
+	for (let j = 0; j < string.length; j++) {
+		const char = string[j];
 		this.glyphs.push({
 			char: char,
 			glyph_canvas: get_glyph_canvas(char),
@@ -135,21 +134,21 @@ function Path(string) {
 	}
 }
 Path.prototype.set_string = function(new_string) {
-	var old_glyphs = this.glyphs;
-	var old_string = this.string;
+	const old_glyphs = this.glyphs;
+	const old_string = this.string;
 
 	this.string = new_string;
 	this.glyphs = [];
 
-	var old_string_index = 0;
-	for (var j = 0; j < new_string.length; j++) {
-		var old_char = old_string[old_string_index];
-		var char = new_string[j];
+	let old_string_index = 0;
+	for (let j = 0; j < new_string.length; j++) {
+		const old_char = old_string[old_string_index];
+		const char = new_string[j];
 		if(char === old_char){
 			this.glyphs.push(old_glyphs[old_string_index]);
 			old_string_index++;
 		}else{
-			var prev_glyph = this.glyphs[this.glyphs.length - 1];
+			const prev_glyph = this.glyphs[this.glyphs.length - 1];
 			this.glyphs.push({
 				char: char,
 				glyph_canvas: get_glyph_canvas(char),
@@ -173,7 +172,7 @@ Path.prototype.set_string = function(new_string) {
 // https://en.wikipedia.org/wiki/Hirschberg%27s_algorithm
 
 // Path.prototype.matchTo = function(string) {
-// 	var dist = Levenshtein.get(this.string, string);
+// 	let dist = Levenshtein.get(this.string, string);
 // 	return dist / (Math.max(this.string.length, string.length) + 1);
 // }
 
@@ -181,11 +180,11 @@ const completely_faded_out_point = 30;
 const fade_out_over = 20;
 
 Path.prototype.simulate = function(matched, place_y, selection_end_pos) {
-	var place_x = 0;
-	for (var j = 0; j < this.glyphs.length; j++) {
-		var glyph = this.glyphs[j];
-		var prev_glyph = this.glyphs[j - 1];
-		var glyph_canvas = glyph.glyph_canvas
+	let place_x = 0;
+	for (let j = 0; j < this.glyphs.length; j++) {
+		const glyph = this.glyphs[j];
+		const prev_glyph = this.glyphs[j - 1];
+		// const glyph_canvas = glyph.glyph_canvas
 		if(prev_glyph){
 			// either of these will work normally as before
 			// place_x = place_x + prev_glyph.glyph_canvas.glyph_width;
@@ -220,8 +219,8 @@ Path.prototype.simulate = function(matched, place_y, selection_end_pos) {
 }
 
 function resize() {
-	var width = window.innerWidth;
-	var height = window.innerHeight;
+	const width = window.innerWidth;
+	const height = window.innerHeight;
 	if (canvas.width !== width) canvas.width = width;
 	if (canvas.height !== height) canvas.height = height;
 }
@@ -232,26 +231,26 @@ function animate(t) {
 	
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.font = font_size + "px/" + line_height + "px Arial";
+
+	const text = input.value;
 	
-	var text = input.value;
-	
-	var start_pos = input.selectionStart;
-	var end_pos = input.selectionEnd;
+	const start_pos = input.selectionStart;
+	const end_pos = input.selectionEnd;
 	// NOTE: these are already min and max;
 	// there's selectionDirection which can be "forward", "backward", or "none"
 	
-	var lower_pos = Math.min(start_pos, end_pos);
-	var upper_pos = Math.max(start_pos, end_pos);
+	const lower_pos = Math.min(start_pos, end_pos);
+	const upper_pos = Math.max(start_pos, end_pos);
 	
-	var before = text.slice(0, lower_pos);
-	var inside = text.slice(lower_pos, end_pos);
-	var after = text.slice(end_pos);
+	const before = text.slice(0, lower_pos);
+	const inside = text.slice(lower_pos, end_pos);
+	const after = text.slice(end_pos);
 	
-	// var all_text_metrics = ctx.measureText(text);
+	// const all_text_metrics = ctx.measureText(text);
 	// console.log(all_text_metrics.fontBoundingBoxDescent);
-	var before_width = ctx.measureText(before).width;
-	var inside_width = ctx.measureText(inside).width;
-	var after_width = ctx.measureText(after).width;
+	const before_width = ctx.measureText(before).width;
+	const inside_width = ctx.measureText(inside).width;
+	const after_width = ctx.measureText(after).width;
 	
 	if (text !== previous_text || end_pos !== previous_selection_end || end_pos !== start_pos) {
 		cursor_blink_on = true;
@@ -277,8 +276,8 @@ function animate(t) {
 	ctx.fillText(inside, before_width, 0);
 	ctx.fillStyle = "black";
 	ctx.fillText(after, before_width + inside_width, 0);
-	
-	var input_focused = document.activeElement === input && document.hasFocus();
+
+	const input_focused = document.activeElement === input && document.hasFocus();
 	if (cursor_blink_on && input_focused) {
 		if (end_pos === start_pos) {
 			ctx.fillRect(before_width, -font_size, 2, line_height);
@@ -289,38 +288,38 @@ function animate(t) {
 		// 	ctx.fillRect(before_width + inside_width, -font_size, 2, line_height);
 		// }
 	}
-	// for (var i = 0; i < paths.length; i++) {
-	// 	var path = paths[i];
+	// for (let i = 0; i < paths.length; i++) {
+	// 	let path = paths[i];
 	// 	ctx.translate(0, line_height);
 	// 	ctx.fillText(path.str, 0, 0);
 	// }
 	
-	var place_y = 0;
-	for (var i = 0; i < paths.length; i++) {
-		var path = paths[i];
-		// var place_y = (1 + i) * line_height;
+	let place_y = 0;
+	for (let i = 0; i < paths.length; i++) {
+		const path = paths[i];
+		// let place_y = (1 + i) * line_height;
 		// console.log(path.matchTo(text))
 		// if (path.matchTo(text) > 0.9) {
 		// 	place_y = innerHeight;
 		// }
 		
-		// var str_dist = Levenshtein.get(path.string, text);
+		// let str_dist = Levenshtein.get(path.string, text);
 		
 		// TODO: uniquify truncated strings,
 		// and probably weigh paths higher if there are multiple results for it
 		// and/or visually indicate that case specifically somehow
 		// TODO: actually match paths together and show them branching off
 		// if (path.string.toLowerCase().indexOf(text.toLowerCase()) === 0) {
-		var matched = path.string.toLowerCase().indexOf(text.toLowerCase()) === 0;
+		const matched = path.string.toLowerCase().indexOf(text.toLowerCase()) === 0;
 		path.simulate(matched, place_y, upper_pos);
 		// ctx.rotate(0.04);
 		ctx.rotate(0.04 * (path.glyphs[0] && path.glyphs[0].alpha));
-		for (var j = 0; j < path.glyphs.length; j++) {
+		for (let j = 0; j < path.glyphs.length; j++) {
 			// ctx.rotate(0.001);
 			// ctx.save();
 			// ctx.rotate(-0.1);
-			var glyph = path.glyphs[j];
-			var glyph_canvas = glyph.glyph_canvas;
+			const glyph = path.glyphs[j];
+			const glyph_canvas = glyph.glyph_canvas;
 			ctx.globalAlpha = glyph.alpha;
 			// ctx.rotate(0.002 * glyph.alpha);
 			// ctx.rotate(-0.002 * glyph.alpha * (1 + 0.1 * (i%10)));
